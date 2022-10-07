4a597357f4cf0011a96269f22b11391adc1c9676
chamchimayo <chamchi.maayo@gmail.com>
2022-10-07 14:45:36 +0900

    feat: 할 일 삭제 API 구현 완료

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ mongoose.connect("mongodb://localhost/todo-demo", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 
@@ -17,12 +18,27 @@ router.get("/", (req, res) => {
   res.send("Hi!");
 });
 
+// 할 일 삭제 API
+router.delete("/todos/:todoId", async (req, res) => {
+    const { todoId } = req.params;
+    
+    try {
+        await Todo.findOneAndDelete({ _id : todoId });
+        
+        res.json({ "message": "할 일이 삭제되었습니다." });
+    } catch(error) {
+        res.status(400).json({ errorMessage: "todo 삭제 실패 "});
+    } 
+});
+
+// 할 일 목록 가져오기 API
 router.get("/todos", async (req, res) => {
     const todos = await Todo.find().sort("-order").exec();
 
     res.send({ todos });
 });
 
+// 할 일 추가 API
 router.post("/todos", async (req, res) => {
     const { value } = req.body;
     const maxOrderTodo = await Todo.findOne().sort("-order").exec();
